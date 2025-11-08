687b68bcb428e8c03ca694246d08a70b5cab3637
irorere juliet <irorerejuliet@gmail.com>
2025-11-08 21:55:19 +0100

    button

--- src/Components/customcard/CoreConcepts.jsx
+++ src/Components/customcard/CoreConcepts.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const CoreConcepts = ({image, title, description}) => {
   return (
-    <div className='text-white'>
+    <div className='text-white bg-pink-600 '>
       <li>
       <img src={image} alt="star" className='w-30'/>
       <h2 className='ml-16'>{title} </h2>
